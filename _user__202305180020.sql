9888861e5aed04552f640b5ea9c58784a44ee3fd
sunny12121 <pandeysunny01@gmail.com>
2023-05-18 00:47:55 +0530

    Add files via upload

---
+++ _user__202305180020.sql
@@ -0,0 +1,3 @@
+INSERT INTO library.`user` (username,password,user_type) VALUES
+	 ('monu','$2a$12$.74jxccHuVWUkVOL91as0eKY1SpfqYMTumAEvowGSl.QTIbBprJHy','User'),
+	 ('sunny','$2a$12$.74jxccHuVWUkVOL91as0eKY1SpfqYMTumAEvowGSl.QTIbBprJHy','Admin');
